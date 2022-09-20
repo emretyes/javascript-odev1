5cf4c467d6aaac83d5a5ea1d3ad1875ffe19aeaf
Emre Saglam <labtec_emre@outlook.com>
2022-09-20 21:35:39 +0300

    first commit

---
+++ js/odev.js
@@ -0,0 +1,26 @@
+let username = prompt("Adınızı Giriniz:");
+let myName = document.querySelector("#myName");
+
+myName.innerHTML = `${
+  username.length > 0 ? username : "Kullanıcı Adı Bulunamadı"
+}`;
+
+function showTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(showTime, 1000);
+        
+}
+      
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    };  // add zero in front of numbers < 10
+    return i;
+}
+showTime();
